b98208d75532bc54eb17084814f6c00bb2fe5a92
Filipe Lamego <lamego@debian-lamego.debian-linux>
2024-09-13 16:07:53 -0300

    Refatoração do código em api.php

--- routes/api.php
+++ routes/api.php
@@ -8,32 +8,19 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-Route::post('tokens/create', function (Request $request) {
-    $token = $request->user()->createToken($request->token_name);
-
-    return [
-        'token' => $token->plainTextToken,
-    ];
+//Rota de Teste
+Route::any('ping', function () {
+    return ['pong' => true];
 });
 
+//Rotas Liberadas
 Route::post('login', [AuthController::class, 'login']);
 
 Route::post('register', [UserController::class, 'register']);
 
-Route::get('pagina1', function () {
-    return 'Pagina 1';
-});
-
+//Rotas Protegidas
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('me', [AuthController::class, 'me']);
     Route::get('logout', [AuthController::class, 'logout']);
 });
 
-//Rota de Teste
-Route::any('ping', function () {
-    return ['pong' => true];
-});

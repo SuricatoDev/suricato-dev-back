027105d24b83a5c8d7d5c535eb0c1ec46bd8b08c
filipelamego <filipeslamego@gmail.com>
2025-03-14 21:52:32 -0300

    :recycle: refactor: Correção de rotas para usuários e documentação swagger

--- app/Http/Controllers/CepController.php
+++ app/Http/Controllers/CepController.php
@@ -7,6 +7,51 @@ use Illuminate\Support\Facades\Http;
 
 class CepController extends Controller
 {
+
+    /**
+     * @OA\Get(
+     *     path="/api/cep/{cep}",
+     *     summary="Buscar informações de um CEP",
+     *     description="Este endpoint consulta o ViaCEP para retornar informações sobre um determinado CEP.",
+     *     tags={"CEP"},
+     *     @OA\Parameter(
+     *         name="cep",
+     *         in="path",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="string",
+     *             example="01001000",
+     *             description="CEP a ser consultado"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Informações do CEP encontradas com sucesso.",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="cep", type="string", example="01001-000", description="CEP encontrado"),
+     *             @OA\Property(property="logradouro", type="string", example="Praça da Sé", description="Logradouro do CEP"),
+     *             @OA\Property(property="bairro", type="string", example="Centro", description="Bairro do CEP"),
+     *             @OA\Property(property="cidade", type="string", example="São Paulo", description="Cidade do CEP"),
+     *             @OA\Property(property="uf", type="string", example="SP", description="UF do CEP")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="CEP não encontrado.",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="message", type="string", example="CEP não encontrado.")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=500,
+     *         description="Erro ao buscar o CEP.",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="message", type="string", example="Erro ao buscar o CEP. Tente novamente.")
+     *         )
+     *     )
+     * )
+     */
+
     public function buscarCep($cep)
     {
         // Faz a requisição para a API do ViaCEP

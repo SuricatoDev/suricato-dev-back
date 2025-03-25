98e0af53ac2bc6f1e0f80bff40f3c473a42be286
filipelamego <filipeslamego@gmail.com>
2025-03-25 20:14:37 -0300

    :recycle: refactor: Ajustes na tabela e nas rotas de caravanas

--- database/migrations/2025_02_27_232151_create_caravanas_table.php
+++ database/migrations/2025_02_27_232151_create_caravanas_table.php
@@ -36,7 +36,6 @@ return new class extends Migration
             $table->foreign('organizador_id')
                 ->references('id')
                 ->on('organizadores');
-            $table->string('categoria');
             $table->timestamps();
         });
     }
